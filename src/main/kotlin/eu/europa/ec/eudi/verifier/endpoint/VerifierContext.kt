niscy-eudiw/eudi-srv-web-verifier-endpoint-23220-4-kt

Add configuration properties for Trust

src/main/kotlin/eu/europa/ec/eudi/verifier/endpoint/VerifierContext.kt
@@ -213,21 +213,22 @@ internal fun beans(clock: Clock) = beans {
 
     // Default ValidateAttestationIssuerTrust
     bean(isLazyInit = true) {
+        val config = ref<AttestationTrustProperties>()
         ValidateAttestationIssuerTrust.usingTrustService(
             ref(),
-            Url(env.getRequiredProperty("verifier.trustService.url")),
-            emptyMap(),
-            ServiceType.EAAProvider,
+            Url(config.serviceUrl),
+            config.attestations.associate { it.attestationType to it.serviceType },
+            config.defaultServiceType,
         )
     }
 
     // Default DeviceResponseValidator
-    bean(isLazyInit = true) { deviceResponseValidator(ref()) }
+    bean { deviceResponseValidator(ref()) }
 
     // Default SdJwtVcValidator
-    bean(isLazyInit = true) { sdJwtVcValidator(ref()) }
+    bean { sdJwtVcValidator(ref()) }
 
-    bean(isLazyInit = true) {
+    bean {
         ValidateMsoMdocDeviceResponse(
             ref(),
             ref(),
@@ -239,7 +240,7 @@ internal fun beans(clock: Clock) = beans {
             },
         )
     }
-    bean(isLazyInit = true) {
+    bean {
         ValidateSdJwtVc(
             sdJwtVcValidatorFactory = { userProvidedRootCACertificates ->
                 val appDefault = ref<SdJwtVcValidator>()
@@ -251,7 +252,7 @@ internal fun beans(clock: Clock) = beans {
         )
     }
 
-    bean(isLazyInit = true) {
+    bean {
         ValidateSdJwtVcOrMsoMdocVerifiablePresentation(
             config = ref(),
             sdJwtVcValidatorFactory = { userProvidedRootCACertificates ->
@@ -763,5 +764,17 @@ internal data class TypeMetadataResolutionProperties(
     data class IntegrityProperties(
         val enabled: Boolean = false,
         val allowedAlgorithms: Set<IntegrityAlgorithm> = IntegrityAlgorithm.entries.toSet(),
+    )
+}
+
+@ConfigurationProperties("verifier.trust")
+internal data class AttestationTrustProperties(
+    val serviceUrl: String,
+    val attestations: List<AttestationProperties>,
+    val defaultServiceType: ServiceType = ServiceType.EAAProvider,
+) {
+    data class AttestationProperties(
+        val attestationType: String,
+        val serviceType: ServiceType,
     )
 }

eu-digital-identity-wallet/eudi-srv-verifier-endpoint

src/main/kotlin/eu/europa/ec/eudi/verifier/endpoint/VerifierContext.kt
@@ -208,11 +208,12 @@ internal fun beans(clock: Clock) = BeanRegistrarDsl {
 
     // Default ValidateAttestationIssuerTrust
     registerBean(lazyInit = true) {
+        val config = bean<AttestationTrustProperties>()
         ValidateAttestationIssuerTrust.usingTrustService(
             bean(),
-            Url(env.getRequiredProperty("verifier.trustService.url")),
-            emptyMap(),
-            ServiceType.EAAProvider,
+            Url(config.serviceUrl),
+            config.attestations.associate { it.attestationType to it.serviceType },
+            config.defaultServiceType,
         )
     }
 
@@ -675,5 +676,17 @@ internal data class TypeMetadataResolutionProperties(
     data class IntegrityProperties(
         val enabled: Boolean = false,
         val allowedAlgorithms: Set<IntegrityAlgorithm> = IntegrityAlgorithm.entries.toSet(),
+    )
+}
+
+@ConfigurationProperties("verifier.trust")
+internal data class AttestationTrustProperties(
+    val serviceUrl: String,
+    val attestations: List<AttestationProperties>,
+    val defaultServiceType: ServiceType = ServiceType.EAAProvider,
+) {
+    data class AttestationProperties(
+        val attestationType: String,
+        val serviceType: ServiceType,
     )
 }